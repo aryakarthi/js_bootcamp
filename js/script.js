d1aa2f9ca96e817f8f0a980d142224fbab348dfc
aryakarthi <aryaperumal.m@gmail.com>
2023-01-26 21:17:17 +0530

    Updated objects

--- js/script.js
+++ js/script.js
@@ -322,13 +322,238 @@
 // }
 // console.log(temp);
 
+// todo - Merge the two arrays. union([1, 2, 3], [100, 2, 1, 10]);
+// todo - Output: [1, 2, 3, 100, 2, 1, 10]
+
+// const array1 = [1, 2, 3];
+// const array2 = [100, 2, 1, 10];
+
+// for(let i = 0; i < array2.length; i++) {
+//   array1.push(array2[i])
+// }
+
+// console.log(array1);
+
+// ? Objects
+
+// let stud1 = { 
+//   tamil:96, 
+//   english:90,
+//   maths:88,
+//   physics:84,
+//   chemistry:80,
+//   computer_science:86,
+//   pre_marks:[92,85,94,86,75,90],
+//   total_marks:function(){
+//     return (this.tamil + this.english + this.maths + this.physics + this.chemistry + this.computer_science);
+//   },
+//   aveg_marks:function(){
+//     return (this.total_marks()/6);
+//   },
+//   pre_total:function(){
+//     let n = 0;
+//     for(let i = 0; i < this.pre_marks.length; i++) {
+//       n += this.pre_marks[i];
+//     }
+//     return n;
+//   },
+//   pre_aveg:function(){
+//     return (this.pre_total()/6);
+//   }
+// }
+// let total = stud1.total_marks();
+// let aveg = stud1.aveg_marks();
+// let preTotal = stud1.pre_total();
+// let preAveg = stud1.pre_aveg();
+// console.log("Total Marks: " +total);
+// console.log("Average: " +aveg);
+// console.log("Previous Total Marks: " +preTotal);
+// console.log("Previous Average: " +preAveg);
 
 
+let prod1 = { 
+  product:"Mobile", 
+  brand:"Apple",
+  model:"iPhone 13",
+  price:61999,
+  color:"black",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod2 = { 
+  product:"Mobile", 
+  brand:"OnePlus",
+  model:"OnePlus-10Pro",
+  price:66999,
+  color:"black",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod3 = { 
+  product:"Mobile", 
+  brand:"Redmi",
+  model:"Redmi Note-11Pro",
+  price:18999,
+  color:"blue",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod4 = { 
+  product:"Mobile", 
+  brand:"Realme",
+  model:"Realme Narzo-50Pro",
+  price:21999,
+  color:"blue",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod5 = { 
+  product:"Mobile", 
+  brand:"Poco",
+  model:"Poco X4-Pro",
+  price:17999,
+  color:"black",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod6 = { 
+  product:"Mobile", 
+  brand:"IQOO",
+  model:"IQOO Z6",
+  price:18499,
+  color:"blue",
+  image:"",
+  specifications:["ram","chip","display","memory","camera","battery"]
+};
+let prod7 = { 
+  product:"Power Bank", 
+  brand:"Xiaomi",
+  model:"MI Powerbank-3i",
+  price:2050,
+  color:"blue",
+  image:"",
+  specifications:["charging-speed","outputs","capacity"]
+};
+let prod8 = { 
+  product:"Power Bank", 
+  brand:"Ambrane",
+  model:"Ambrane 20000",
+  price:1799,
+  color:"black",
+  image:"",
+  specifications:["charging-speed","outputs","capacity"]
+};
+let prod9 = { 
+  product:"Power Bank", 
+  brand:"URBN",
+  model:"URBN 20000",
+  price:1999,
+  color:"green",
+  image:"",
+  specifications:["charging-speed","outputs","capacity"]
+};
+let prod10 = { 
+  product:"Earbuds", 
+  brand:"Samsung",
+  model:"Samsung Galaxy Buds",
+  price:5250,
+  color:"black",
+  image:"",
+  specifications:["playtime","connectivity","controls"]
+};
+let prod11 = { 
+  product:"Earbuds", 
+  brand:"boAt",
+  model:"boAt Airdopes-141",
+  price:1399,
+  color:"blue",
+  image:"",
+  specifications:["playtime","connectivity","controls"]
+};
+let prod12 = { 
+  product:"Smartwatch", 
+  brand:"Fastrack",
+  model:"Fastrack Reflex",
+  price:2499,
+  color:"black",
+  image:"",
+  specifications:["sports-mode","display","battery"]
+};
+let prod13 = { 
+  product:"Smartwatch", 
+  brand:"Boult",
+  model:"Boult Dive",
+  price:1999,
+  color:"blue",
+  image:"",
+  specifications:["sports-mode","display","battery"]
+};
+let prod14 = { 
+  product:"Smartwatch", 
+  brand:"boAt",
+  model:"boAt Xtend",
+  price:2299,
+  color:"black",
+  image:"",
+  specifications:["sports-mode","display","battery"]
+};
+let prod15 = { 
+  product:"Smartwatch", 
+  brand:"Noise",
+  model:"Noise ColorFit",
+  price:1899,
+  color:"blue",
+  image:"",
+  specifications:["sports-mode","display","battery"]
+};
+let prod16 = { 
+  product:"Laptop", 
+  brand:"HP",
+  model:"HP 15s",
+  price:45000,
+  color:"silver",
+  image:"",
+  specifications:["ram","chip","display","memory","battery"]
+};
+let prod17 = { 
+  product:"Laptop", 
+  brand:"Lenovo",
+  model:"Lenovo Ideapad",
+  price:46999,
+  color:"black",
+  image:"",
+  specifications:["ram","chip","display","memory","battery"]
+};
+let prod18 = { 
+  product:"Laptop", 
+  brand:"Asus",
+  model:"Asus Vivobook",
+  price:47000,
+  color:"silver",
+  image:"",
+  specifications:["ram","chip","display","memory","battery"]
+};
+let prod19 = { 
+  product:"Laptop", 
+  brand:"Dell",
+  model:"Dell Vostro",
+  price:58500,
+  color:"silver",
+  image:"",
+  specifications:["ram","chip","display","memory","battery"]
+};
+let prod20 = { 
+  product:"Laptop", 
+  brand:"Acer",
+  model:"Acer Aspire 5",
+  price:59800,
+  color:"black",
+  image:"",
+  specifications:["ram","chip","display","memory","battery"]
+};
 
-
-
-
-
+let products = [prod1,prod2,prod3,prod4,prod5,prod6,prod7,prod8,prod9,prod10,prod11,prod12,prod13,prod14,prod15,prod16,prod17,prod18,prod19,prod20];
+console.log(products);
 
 
 
